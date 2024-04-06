728b951a32eb7f1eeb24b636d64ee76a20957322
Evgeniy Smirnov <59017716+mrlimax1@users.noreply.github.com>
2024-04-07 00:14:49 +0500

    🛟 review

--- src/app/api/routes/places.py
+++ src/app/api/routes/places.py
@@ -1,20 +1,20 @@
 import json
 from typing import Any
-from fastapi import APIRouter, HTTPException, status, Response
-from src.database.controllers import PlacesController
+from fastapi import APIRouter, Response
+from src.database import Database
 
 router = APIRouter()
 
 
 @router.get("/get", description="Get list of places")
 async def get():
-    places = await PlacesController.get_all()
+    places = await Database.places.get_all()
     return json.dumps(places)
 
 
 @router.get("/getById", description="Get information about place")
 async def get_by_id(place_id: int):
-    current_place = PlacesController.find_by_id(place_id)
+    current_place = Database.places.find_by_id(place_id)
     return json.dumps(current_place)
 
 
@@ -30,49 +30,14 @@ async def add() -> Any:
 
 @router.post("/remove", description="remove place (special method)")
 async def remove(moderator_key, place_id):
-    if moderator_key:  # todo: find moderator key
-        await PlacesController.remove(place_id)
-        return Response(status_code=200)
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Access denied"
-        )
+    await Database.places.remove(place_id)
+    return Response(status_code=200)
 
 
 @router.post("/edit", description="edit place (special method)")
-async def edit(moderator_key, place_id, title=None, description=None, category=None, filters=None):
-    if moderator_key:  # todo: find moderator key
-        place = await PlacesController.find_by_id(place_id)
-
-        if title is not None:
-            place.title = title
-        if description is not None:
-            place.description = description
-        if category is not None:
-            place.category = category
-        if filters is not None:
-            place.filters_list = filters
-
-        return Response(status_code=200)
-
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Access denied"
-        )
-
-
-@router.post("/like", description="Get user's report")
-async def get(moderator_key):
-    if moderator_key:  # todo: find moderator key
-        ...
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Access denied"
-        )
-
+async def edit(moderator_key: str, place_id: int, title=None, description=None, category=None, filters=None):
+    await Database.places.update(place_id, title, description, category, filters)
+    return Response(status_code=200)
 
 @router.post("/like", description="Add place in user liked list")
 async def like():
--- src/app/api/routes/reviews.py
+++ src/app/api/routes/reviews.py
@@ -1,15 +1,20 @@
 import json
 from typing import Any
 from fastapi import APIRouter
-from src.database.controllers import PlacesController
+from src.database import Database
 
 router = APIRouter()
 
 
 @router.get("/getByPlaceId", description="Get reviews by place_id")
 async def get_by_place_id(place_id: int):
-    current_place = await PlacesController.find_by_id(place_id)
-    reviews = current_place.reviews_list
+    current_place = await Database.places.find_by_id(place_id)
+    reviews_ids = current_place.reviews_list
+
+    reviews = []
+
+    for review_id in reviews_ids:
+        reviews.append(await Database.reviews.find_by_id(review_id))
     return json.dumps(reviews)
 
 
