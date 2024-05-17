09e1f47243133a6a0509664a43f54fc611f7f19d
= <mrfrostlikchannel@gmail.com>
2024-05-17 17:00:57 +0500

    fix

--- src/app/extensions/photo_resize.py
+++ src/app/extensions/photo_resize.py
@@ -1,3 +1,4 @@
+from base64 import b64encode
 from io import BytesIO
 from PIL import Image
 from PIL.Image import Resampling
@@ -20,4 +21,6 @@ def make_thumbnail(photo, size=(512, 512)):
     img.thumbnail(new_size, Resampling.LANCZOS)
     img.save(buffer, format=photo_data[1][6:].upper())
 
-    return 
+    byte_data = buffer.getvalue()
+    return b64encode(byte_data).decode('utf-8')
+     
